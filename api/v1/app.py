a901841e395fcc7eae20b888e07f2a8c03ae2615
Razeetech <rajiazeezsch@gmail.com>
2023-07-31 13:48:04 -0700

    AirBnB update

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
+"""Flask server (variable app)
 """
-module to create Flask app that works with the API
-"""
-from models import storage
+
+
 from flask import Flask, jsonify
-from flask_cors import CORS
+from models import storage
+from os import getenv
 from api.v1.views import app_views
-import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-app.config.update(JSONIFY_PRETTYPRINT_REGULAR=True)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def tear_down(self):
-    """
-    tears down
-    """
+def downtear(self):
+    '''Status of your API'''
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """
-    error handler for 404
-    """
-    return (jsonify({'error': 'Not found'}), 404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error='Not found'), 404
 
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
